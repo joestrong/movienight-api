698e9bbc26d94d5668ef277e21e7e6fffc4d42b4
Joe Strong <joestrong@gmail.com>
2017-10-08 17:13:10 +0100

    Initial commit

---
+++ app/Http/Controllers/MoviesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class MoviesController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->movies = new \App\Services\TheMoviesDatabaseMoviesService();
+    }
+
+    public function index()
+    {
+        $movies = $this->movies->getTop(30);
+        $return = [];
+        foreach ($movies as $movie) {
+            array_push($return, $movie->getTitle());
+        }
+        return response()->json($return);
+    }
+}
---
+++ app/Services/TheMoviesDatabaseMoviesService.php
@@ -0,0 +1,33 @@
+<?php namespace App\Services;
+
+class TheMoviesDatabaseMoviesService
+{
+
+    protected $client;
+    protected $repository;
+
+    public function __construct()
+    {
+        $token  = new \Tmdb\ApiToken('4face5a64cfbebe80a03929b77b1576c');
+        $this->client = new \Tmdb\Client($token);
+        $this->repository = new \Tmdb\Repository\MovieRepository($this->client);
+    }
+
+    public function getTop($count = 10)
+    {
+        return $this->repository->getPopular();
+    }
+
+    public function search($queryText)
+    {
+        $query = new \Tmdb\Model\Search\SearchQuery\MovieSearchQuery();
+        $query->page(1);
+        $repository = new \Tmdb\Repository\SearchRepository($this->client);
+        return $repository->searchMovie($queryText, $query);
+    }
+
+    public function get($id)
+    {
+        return $this->repository->load($id);
+    }
+}
