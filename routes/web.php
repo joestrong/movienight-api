a8c2fbfdec493bc07ac97dc3d2e21109d185211b
Joe Strong <joestrong@gmail.com>
2019-04-06 17:38:43 +0100

    Adds router group to simplify auth

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,16 @@
 |
 */
 
+use Laravel\Lumen\Routing\Router;
+
 $router->get('/', function () use ($router) {
     return response()->json([]);
 });
 
 $router->post('auth/exchange/facebook', 'LoginController@exchangeFacebookToken');
 
-$router->get('/movies', [
-    'middleware' => 'auth',
-    'uses' => 'MoviesController@index'
-]);
+$router->group(['middleware' => 'auth'], function(Router $router) {
+    $router->get('/movies', [
+        'uses' => 'MoviesController@index'
+    ]);
+});
